98eabf072db79f015d61f93525c9b8b0419a9b15
mnbarber <mnbarber1993@gmail.com>
2022-06-03 19:16:05 -0700

    added update, delete, create options, working on second model

--- main_app/models.py
+++ main_app/models.py
@@ -11,3 +11,12 @@ class Author(models.Model):
 
     class Meta:
         ordering = ['name']
+
+class Book(models.Model):
+    title = models.CharField(max_length=150)
+    length = models.IntegerField(default=0)
+    cover = models.CharField(max_length=500)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
+
+    def __str__(self):
+        return self.title
--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.Home.as_view(), name="home"),
     path('about/', views.About.as_view(), name="about"),
-    path('authors/', views.AuthorList.as_view(), name="author_list")
+    path('authors/', views.AuthorList.as_view(), name="author_list"),
+    path('authors/new/', views.AuthorCreate.as_view(), name="author_create"),
+    path('authors/<int:pk>/', views.AuthorDetail.as_view(), name="author_detail"),
+    path('authors/<int:pk>/update', views.AuthorUpdate.as_view(), name="author_update"),
+    path('authors/<int:pk>/delete', views.AuthorDelete.as_view(), name="author_delete")
 ]
